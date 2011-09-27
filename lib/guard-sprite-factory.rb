20e70db206d7becd9b0f6a33694604c6599d0cc2
Christopher Hein <me@christopherhein.com>
2011-09-27 16:59:24 -0700

    initial commit

---
+++ lib/guard-sprite-factory.rb
@@ -0,0 +1,9 @@
+require "guard-sprite-factory/version"
+
+module Guard
+  module Sprite
+    module Factory
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/guard-sprite-factory/version.rb
@@ -0,0 +1,7 @@
+module Guard
+  module Sprite
+    module Factory
+      VERSION = "0.0.1"
+    end
+  end
+end
